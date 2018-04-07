5623792c4c62804fcb805bc7f15781afe48b8c65
Ken <none>
2018-04-07 11:10:10 -0400

    Refactored for efficiency.
    Get rid of reverse/sort each line and print basic values to stdout.

--- tally.py
+++ tally.py
@@ -17,6 +17,9 @@ def read_extensions(txt_file):
     # FIXME fixed format of Extensions.txt file assumed, newline separated extensions
     with open(ext_file) as fh:
         ext_lines = fh.read().splitlines()
+
+    print '%d extensions found in Extensions.txt file' % len(ext_lines)
+
     return tuple(ext_lines)
 
 
@@ -31,39 +34,38 @@ def write_tally_csv_file(fname, exts):
     # read file into raw lines list
     with open(fname) as f:
         raw_lines = f.read().splitlines()
+    print '%d lines read from %s' % (len(raw_lines), fname)
 
     # right strip each line
     rstrip_lines = [raw_line.rstrip() for raw_line in raw_lines]
 
     # keep only lines that end with one of the extensions we specified
-    ext_lines = [s for s in rstrip_lines if s.endswith(exts)]
-
-    # reverse matching ext lines and sort (going backwards ftw)
-    rev_lines = [s[::-1] for s in ext_lines]
-    rev_lines.sort()
-
-    # reverse each line again, so now their frontwards
-    lines = [x[::-1] for x in rev_lines]
+    kept_lines = [s for s in rstrip_lines if s.endswith(exts)]
 
     # iterate over extensions to count total for each and dupes too
     headers = []
     values = []
     for ext in exts:
         headers += [ext, ext + '_dupes']
-        ext_lines = [a for a in lines if a.endswith(ext)]
+        ext_lines = [a for a in kept_lines if a.endswith(ext)]
         c = Counter(ext_lines)
-        num_unique = len(c)
-        num_total = sum(c.values())
-        num_dupes = num_total - num_unique
+        num_unique = len(c)  # unique lines for this ext
+        num_total = sum(c.values())  # total count for this ext
+        num_dupes = num_total - num_unique  # dupe count for this ext
         values += [num_total, num_dupes]
 
+    # write results to output csv file
     with open(csv_file, "w") as text_file:
         text_file.write(','.join(headers))
-        text_file.write('\n' + ','.join([str(i) for i in values]))
+        text_file.write('\n' + ','.join([str(i) for i in values]) + '\n')
 
 
 def main(input_file):
-    """"""
+    """tally lines (and dupes) associated with specified extensions
+    - read extensions from Extensions.txt file, assumed to co-exist with input file
+    - use extensions to process input file
+    - write results to csv output file; same name as input but extension changed
+    """
     extensions = read_extensions(input_file)
     write_tally_csv_file(input_file, extensions)
 
